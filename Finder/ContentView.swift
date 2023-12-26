252e976c83d0598276afe94fa875d8e370f327b6
sanburnn <dimas00758@gmail.com>
2023-12-26 22:23:44 +0700

    add item

--- Finder/ContentView.swift
+++ Finder/ContentView.swift
@@ -8,19 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, Dimas yudha!")
+  
+  @State private var newTodo = ""
+  @State private var allTodos: [TodoItem] = []
+  private let todosKey = "todosKey"
+  
+  var body: some View {
+    NavigationView {
+      VStack {
+        HStack {
+          TextField("Add todo...", text: $newTodo)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+
+          Button(action: {
+            guard !self.newTodo.isEmpty else { return }
+            self.allTodos.append(TodoItem(todo: self.newTodo))
+            self.newTodo = ""
+            self.saveTodos()
+          }) {
+            Image(systemName: "plus")
+          }
+          .padding(.leading, 5)
+        }.padding()
+
+        List {
+          ForEach(allTodos) { todoItem in
+            Text(todoItem.todo)
+          }.onDelete(perform: deleteTodo)
         }
-        .padding()
+      }
+      .navigationBarTitle("Todos")
+    }.onAppear(perform: loadTodos)
+  }
+
+  private func saveTodos() {
+    UserDefaults.standard.set(try? PropertyListEncoder().encode(self.allTodos), forKey: todosKey)
+  }
+
+  private func loadTodos() {
+    if let todosData = UserDefaults.standard.value(forKey: todosKey) as? Data {
+      if let todosList = try? PropertyListDecoder().decode(Array<TodoItem>.self, from: todosData) {
+        self.allTodos = todosList
+      }
     }
+  }
+
+  private func deleteTodo(at offsets: IndexSet) {
+    self.allTodos.remove(atOffsets: offsets)
+    saveTodos()
+  }
+}
+
+struct TodoItem: Codable, Identifiable {
+  let id = UUID()
+  let todo: String
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
