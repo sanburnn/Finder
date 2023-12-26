7d0361b35b19e30043551155f554753ea463ae1c
sanburnn <dimas00758@gmail.com>
2023-12-26 21:59:08 +0700

    Initial Commit

---
+++ Finder/FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinderApp.swift
+//  Finder
+//
+//  Created by dimas maulana firdaus on 26/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
