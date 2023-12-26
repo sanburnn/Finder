0d91d2a7e785cc52be7a9cda464d3720423111d7
sanburnn <dimas00758@gmail.com>
2023-12-26 22:06:09 +0700

    check

--- Finder/ContentView.swift
+++ Finder/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Dimas!")
+            Text("Hello, Dimas yudha!")
         }
         .padding()
     }
